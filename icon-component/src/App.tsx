f5adc9d6fb3c0678a6a26a5325c18fa31fc81491
hanbing1991 <hblx2017@163.com>
2025-11-06 17:06:33 +0800

    test createFrontIconFont

--- icon-component/src/App.tsx
+++ icon-component/src/App.tsx
@@ -1,7 +1,10 @@
 
 import './App.css'
+import createFrontIconFont from './Icon/createFrontIconFont'
 import IconAdd from './Icon/icons/IconAdd'
 import IconEmail from './Icon/icons/IconEmail'
+
+const IconFont = createFrontIconFont('//at.alicdn.com/t/c/font_5059178_3z7ra0ql20n.js')
 
 function App() {
   return (
@@ -9,6 +12,9 @@ function App() {
       <IconAdd size='40px'></IconAdd>
       <IconEmail spin></IconEmail>
       <IconEmail style={{color: 'blue', fontSize: '50px'}}></IconEmail>
+      <hr />
+      <IconFont type='icon-a-yuangongfuli2x' fill="red" size='60px'/>
+      <IconFont type='icon-a-yuangongchengchang2x' size='50px'/>
     </div>
   )
 }
